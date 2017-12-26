456704f17958455c38c3c4d764fd6db731fccca8
Surge Raval <Surge1223@gmail.com>
2017-12-26 17:59:19 +0000

    taimen: add needed  permissions for carrier priv-apps

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ $(warning Overlays defined in '$(DEVICE_PACKAGE_OVERLAYS)' will override '$(PROD
 endif
 DEVICE_PACKAGE_OVERLAYS += device/google/taimen/overlay
 
+# Default priv-app permissions
+PRODUCT_COPY_FILES += \
+    device/google/taimen/privapp-permissions-taimen.xml:$(TARGET_OUT_ETC)/permissions/privapp-permissions-taimen.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
     device/google/taimen/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
