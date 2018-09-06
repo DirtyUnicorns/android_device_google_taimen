6be9f163393bc6e803f9b222409525e4fa0fdd62
spaceman860 <spaceman8583@gmail.com>
2018-09-06 02:17:50 -0400

    taimen: update fingerprint PPR2.180905.005

--- du.mk
+++ du.mk
@@ -33,8 +33,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys \
-    PRIVATE_BUILD_DESC="taimen-user 9 PPR1.180610.009 4898911 release-keys"
+    BUILD_FINGERPRINT=google/taimen/taimen:9/PPR2.180905.005/4928864:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 9 PPR2.180905.005 4928864 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
