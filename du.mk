5466174891e7235f11a3c204f8d6fd12c1ca419d
ezio84 <brabus84@gmail.com>
2019-04-02 22:15:58 +0200

    Update taimen fingerprint to April image

--- du.mk
+++ du.mk
@@ -33,8 +33,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:9/PQ2A.190305.002/5240760:user/release-keys \
-    PRIVATE_BUILD_DESC="taimen-user 9 PQ2A.190305.002 5240760 release-keys"
+    BUILD_FINGERPRINT=google/taimen/taimen:9/PQ2A.190405.003/5310204:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ2A.190405.003 5310204 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
