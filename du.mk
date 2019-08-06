ea8872f87d2ce919d4cf6e8f49670bb153048418
spaceman860 <spaceman8583@gmail.com>
2019-08-06 16:17:24 +0200

    Update build fingerprint to PQ3A.190801.002

--- du.mk
+++ du.mk
@@ -33,8 +33,8 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:9/PQ3A.190705.001/5565753:user/release-keys \
-    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190705.001 5565753 release-keys"
+    BUILD_FINGERPRINT=google/taimen/taimen:9/PQ3A.190801.002/5670241:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190801.002 5670241 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
